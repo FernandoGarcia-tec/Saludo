66f15bd03923a8214f28e414396441cd87d4699d
Fernando <garciaquintero00@gmail.com>
2025-04-30 16:52:37 -0600

    css a styles

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./css/PrimerComponente.css"
+import "./styles/PrimerComponente.css"
 import fotoPerfil from "./assets/foto.jpg"
 
 export const DatosPersonales = () => {
